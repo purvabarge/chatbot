4d97a1254c56d849aa399a8c67a6d9747889be5e
purvabarge <purva.barge@cumminscollege.in>
2022-08-07 00:25:08 +0530

    First Commit

---
+++ app/src/main/java/com/example/chatbottrial/PersonalDetails.kt
@@ -0,0 +1,11 @@
+package com.example.chatbottrial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PersonalDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_details)
+    }
+}
